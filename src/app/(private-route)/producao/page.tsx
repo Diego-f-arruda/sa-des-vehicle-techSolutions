3a098a26d44f15709092aacf94aacdc125001cb7
MATEUS HENRIQUE DOS SANTOS <mateushsantos07>
2025-04-14 21:30:15 -0300

    14/04

--- src/app/(private-route)/producao/page.tsx
+++ src/app/(private-route)/producao/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/Button'
+import Button from '@/components/Button'
 import './styles.css'
 
 export default function Producao() {
@@ -6,18 +6,33 @@ export default function Producao() {
 
         <div className='producao-container'>
             <div className='input'>
-                <input type="text" id="" placeholder=' Nome do Produto'></input>
+                <input type="text" id="" placeholder=' Modelo do carro'></input>
                 <select name="" id="" >
-                    <option value="">2 portas</option>
+                    <option value="" disabled>Selecione a cor</option>
+                    <option value="" >2 portas</option>
                     <option value="">4 portas</option>
                 </select>
                 <select name="" id="">
+                    <option value="" disabled>Câmbio</option>
                     <option value="">Câmbio Manual</option>
                     <option value="">Câmbio Automático</option>
                 </select>
                 <Button/>
             </div>
+                <p id='paragrafo'>Carros cadastrados anteriormente</p>
+
                 <div className='produtos'>
+                    <h1>aaaaaaaaaaaaaaaaaaa</h1>
+                    <h1>aaaaaaaaaaaaaaaaaaa</h1>
+                    
+                </div>
+                <div className='produtos'>
+                    <h1>aaaaaaaaaaaaaaaaaaa</h1>
+                    <h1>aaaaaaaaaaaaaaaaaaa</h1>
+                    
+                </div>
+                <div className='produtos'>
+                    <h1>aaaaaaaaaaaaaaaaaaa</h1>
                     <h1>aaaaaaaaaaaaaaaaaaa</h1>
                     
                 </div>
