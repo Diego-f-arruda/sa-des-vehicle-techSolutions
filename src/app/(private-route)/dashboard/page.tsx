e2b9b7fddf997828c0d371ae71b3bd5b34a20c60
MATEUS HENRIQUE DOS SANTOS <mateushsantos07>
2025-04-15 20:51:24 -0300

    estilizcaoo

--- src/app/(private-route)/dashboard/page.tsx
+++ src/app/(private-route)/dashboard/page.tsx
@@ -3,7 +3,16 @@ import "./styles.css";
 export default function Dashboard() {
   return (
     <div className="dashboard-container">
-        
-    </div>
-  );
+  <h1>Bem-vindo ao Sistema</h1>
+
+  <div className="recentes">
+    <h2>Atividades Recentes</h2>
+  </div>
+
+  <div>
+    <h4>(Mostrar últimas alterações feitas no sistema)</h4>
+  </div>
+
+</div>
+);
 }
