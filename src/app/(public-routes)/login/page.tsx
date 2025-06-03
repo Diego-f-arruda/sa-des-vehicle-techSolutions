1aef0ec4cd34196458c3135931338657f7549af3
DIEGO FERREIRA ARRUDA <diego_f_arruda@estudante.sesisenai.org.br>
2025-06-02 21:30:13 -0300

    imagem em login

--- src/app/(public-routes)/login/page.tsx
+++ src/app/(public-routes)/login/page.tsx
@@ -3,6 +3,8 @@ import { useRouter } from "next/navigation";
 import styles from "./styles.module.css"
 import { useState } from "react";
 import axios from "axios";
+import fundo from '@/assets/capa.jpg';
+import Image from 'next/image';
 
 export default function Home() {
   const [email, setEmail] = useState<string>("");
@@ -14,47 +16,50 @@ export default function Home() {
 
   async function handleSubmit() {
     try {
-      const loginUrl = "http://localhost:3333/login"; 
+      const loginUrl = "http://localhost:3333/login";
 
-    const response = await axios.post(loginUrl, {
-      email, password
-    });
+      const response = await axios.post(loginUrl, {
+        email, password
+      });
 
       localStorage.setItem('access_token', response.data.access_token)
       router.push('/produto')
     } catch {
-    alert("Erro ao fazer login")
+      alert("Erro ao fazer login")
     }
   }
 
   return (
-    <div className={styles.container}>
-      <div className={styles.form}>
-        <h2>Login</h2>
-        <input
-          type="text"
-          placeholder="E-mail"
-          className={styles.input}
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
+    
+      <div className={styles.container}>
+        <div className={styles.form}>
+          <h2>Login</h2>
+          <input
+            type="text"
+            placeholder="E-mail"
+            className={styles.input}
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
 
-        <input
-          type="password"
-          placeholder="Password"
-          className={styles.input}
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
+          <input
+            type="password"
+            placeholder="Password"
+            className={styles.input}
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
 
-        <button
-          className={styles.button}
-          onClick={handleSubmit}
-          disabled={disabledButton}
-        >
-          Entrar
-        </button>
+          <button
+            className={styles.button}
+            onClick={handleSubmit}
+            disabled={disabledButton}
+          >
+            Entrar
+          </button>
+          <p>Ainda não é cadastrado?    <button type="submit">Cadastre-se</button></p>
+        </div>
       </div>
-    </div>
+    
   );
 }
